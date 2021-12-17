02c3208f5e3c040b57f9faf6823f5aa87d0d712a
jin.hashimoto <h.jin.6621@gmail.com>
2021-12-17 15:28:41 +0000

    kadai-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,11 +14,6 @@
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
-                <tr>
-                    <td>{{ $task->id }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                
                  <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
